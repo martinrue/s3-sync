81c78adc93c2ec5ab07b2b469e789db188b992b1
Martin Rue <hello@martinrue.com>
2019-06-29 22:52:38 +0100

    Add flag parsing in command entrypoint

--- cmd/sync/main.go
+++ cmd/sync/main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	dir     = flag.String("dir", "", "")
+	bucket  = flag.String("bucket", "", "")
+	help    = flag.Bool("help", false, "")
+	version = flag.Bool("version", false, "")
+)
+
+func main() {
+	usageAndExit := func(code int) {
+		fmt.Fprint(os.Stderr, "usage: sync --dir=<directory> --bucket=<bucket>\n")
+		os.Exit(code)
+	}
+
+	flag.Usage = func() {
+		usageAndExit(1)
+	}
+
+	flag.Parse()
+
+	if *help {
+		usageAndExit(0)
+	}
+
+	if *version {
+		fmt.Println("v0.0.1")
+		os.Exit(0)
+	}
+
+	if *dir == "" || *bucket == "" {
+		usageAndExit(1)
+	}
+}
